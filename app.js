8e7700e32417debb12aef169e7e399d8c451dec6
julybell <julybell16@gmail.com>
2021-08-29 09:37:05 +0300

    chain of promises

--- app.js
+++ app.js
@@ -21,20 +21,28 @@ function getUserInfo() {
     return new Promise(function(resolve, reject) {
       setTimeout(() => resolve({ name: 'Vic', age: 21, id: 1 } ), 1000);
     });
-}
+  }
+ 
+  function getUserAvatar(userInfo) {
+    return new Promise(function(resolve, reject) {
+      userInfo.avatar = 'https://previews.123rf.com/images/stockgiu/stockgiu1708/stockgiu170802061/83728179-avatar-sketch-of-a-funny-man-s-haed-with-sunglasses-and-hairstyle-design.jpg'
+      setTimeout(() => resolve(userInfo), 1000);
+    });
+  }
+  function getUserAdditionalInfo(userInfo) {
+    return new Promise(function(resolve, reject) {
+      userInfo.interests = ['sport', 'books'];
+      setTimeout(() => resolve(userInfo), 1000);
+    });
+  }
+
  
 getUserInfo().then((data) => {
 	return data;
 }).then((userInfo) =>{
-	return new Promise(function(resolve, reject) {
-		userInfo.avatar = 'https://previews.123rf.com/images/stockgiu/stockgiu1708/stockgiu170802061/83728179-avatar-sketch-of-a-funny-man-s-haed-with-sunglasses-and-hairstyle-design.jpg'
-		setTimeout(() => resolve(userInfo), 1000);
-	  });
+	return getUserAvatar(userInfo);
 }).then((userInfo) => {
-	return new Promise(function(resolve, reject) {
-		userInfo.interests = ['sport', 'books'];
-		setTimeout(() => resolve(userInfo), 1000);
-	});
+	return getUserAdditionalInfo(userInfo);
 }).then((data) => {
 	console.log(data);
 });
